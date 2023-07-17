2c9a71019e51d3fc34e96f02e5edfe175c0fb30f
gean-k <87350516+gean-k@users.noreply.github.com>
2023-07-17 11:52:53 +0200

    sql

--- etudiants_jee.sql
+++ etudiants_jee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : sam. 08 juil. 2023 à 14:56
+-- Généré le : lun. 17 juil. 2023 à 09:51
 -- Version du serveur : 8.0.30
 -- Version de PHP : 8.2.8
 
@@ -48,10 +48,11 @@ INSERT INTO `classe` (`idClasse`, `libClasse`) VALUES
 --
 
 CREATE TABLE `eleve` (
-  `matElv` varchar(10) NOT NULL,
+  `idEleve` int NOT NULL,
+  `matElv` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL DEFAULT '0000',
   `nomElv` varchar(50) NOT NULL,
   `prenomElv` varchar(50) NOT NULL,
-  `dateNaissElv` date NOT NULL,
+  `dateNaissElv` varchar(10) NOT NULL,
   `lieuNaissElv` varchar(50) NOT NULL,
   `photoElv` varchar(100) NOT NULL,
   `mailElv` varchar(30) NOT NULL,
@@ -63,17 +64,31 @@ CREATE TABLE `eleve` (
 -- Déchargement des données de la table `eleve`
 --
 
-INSERT INTO `eleve` (`matElv`, `nomElv`, `prenomElv`, `dateNaissElv`, `lieuNaissElv`, `photoElv`, `mailElv`, `mdpElv`, `idSexe`) VALUES
-('E001', 'Dupont', 'Jean', '1990-05-15', 'Paris', 'photo1.jpg', 'jean.dupont@example.com', '123456', 1),
-('E002', 'Martin', 'Sophie', '1992-08-22', 'Lyon', 'photo2.jpg', 'sophie.martin@example.com', '654321', 2),
-('E003', 'Dubois', 'Pierre', '1991-11-10', 'Marseille', 'photo3.jpg', 'pierre.dubois@example.com', '987654', 1),
-('E004', 'Lefevre', 'Emma', '1993-02-28', 'Toulouse', 'photo4.jpg', 'emma.lefevre@example.com', '456789', 2),
-('E005', 'Roux', 'Luc', '1990-07-19', 'Nice', 'photo5.jpg', 'luc.roux@example.com', '987123', 1),
-('E006', 'Girard', 'Julie', '1992-10-05', 'Bordeaux', 'photo6.jpg', 'julie.girard@example.com', '321654', 2),
-('E007', 'Moreau', 'Thomas', '1991-03-12', 'Lille', 'photo7.jpg', 'thomas.moreau@example.com', '654987', 1),
-('E008', 'Lecomte', 'Alice', '1993-06-25', 'Strasbourg', 'photo8.jpg', 'alice.lecomte@example.com', '789456', 2),
-('E009', 'Leroy', 'Hugo', '1990-09-08', 'Montpellier', 'photo9.jpg', 'hugo.leroy@example.com', '852369', 1),
-('E010', 'Dupuis', 'Camille', '1992-12-01', 'Rennes', 'photo10.jpg', 'camille.dupuis@example.com', '369852', 2);
+INSERT INTO `eleve` (`idEleve`, `matElv`, `nomElv`, `prenomElv`, `dateNaissElv`, `lieuNaissElv`, `photoElv`, `mailElv`, `mdpElv`, `idSexe`) VALUES
+(1, '0000', 'a', 'z', '2001-05-04', 'ALEPE', 'a.jpg', 'a@a.a', 'azerty', 1),
+(2, 'E001', 'Dupont', 'Jean', '', 'Paris', 'photo1.jpg', 'test@gmail.com', '123456', 1),
+(3, 'E002', 'Martin', 'Sophie', '', 'Lyon', 'photo2.jpg', 'sophie.martin@example.com', '654321', 2),
+(4, 'E003', 'Dubois', 'Pierre', '', 'Marseille', 'photo3.jpg', 'pierre.dubois@example.com', '987654', 1),
+(5, 'E004', 'Lefevre', 'Emma', '', 'Toulouse', 'photo4.jpg', 'emma.lefevre@example.com', '456789', 2),
+(6, 'E005', 'Roux', 'Luc', '', 'Nice', 'photo5.jpg', 'luc.roux@example.com', '987123', 1),
+(7, 'E006', 'Girard', 'Julie', '', 'Bordeaux', 'photo6.jpg', 'julie.girard@example.com', '321654', 2),
+(8, 'E007', 'Moreau', 'Thomas', '', 'Lille', 'photo7.jpg', 'thomas.moreau@example.com', '654987', 1),
+(9, 'E008', 'Lecomte', 'Alice', '', 'Strasbourg', 'photo8.jpg', 'alice.lecomte@example.com', '789456', 2),
+(10, 'E009', 'Leroy', 'Hugo', '', 'Montpellier', 'photo9.jpg', 'hugo.leroy@example.com', '852369', 1),
+(11, 'E010', 'Dupuis', 'Camille', '', 'Rennes', 'photo10.jpg', 'camille.dupuis@example.com', '369852', 2),
+(12, '0000', 'asd', 'zs', '2008-05-04', 'ALsEPE', 'ad.jpg', 'ab@ab.ab', 'azty', 2),
+(13, '0000', 'asd', 'zs', '2008-05-04', 'ALsEPE', 'ad.jpg', 'ab@ab.ab', 'azty', 2),
+(14, '0000', 'a', 'z', '2001-05-04', 'ALEPE', 'a.jpg', 'a@a.a', 'azerty', 1),
+(15, '0000', 'asd', 'zs', '2008-05-04', 'ALsEPE', 'ad.jpg', 'ab@ab.ab', 'azty', 2),
+(16, '0000', 'asd', 'zs', '2008-05-04', 'ALsEPE', 'ad.jpg', 'ab@ab.ab', 'azty', 2),
+(17, '0000', 'asd', 'zs', '2008-05-04', 'ALsEPE', 'ad.jpg', 'ab@ab.ab', 'azty', 2),
+(18, 'ccc', 'koulibaly', 'Ange', '2023-07-28', 'Bondoukou', 'détailVoiture.png', 'c@c.c', 'azert', 1),
+(19, 'betsa', 'gongoua', 'zouzoua', '2023-08-08', 'man', 'IconeRecerche.jpeg', 'z@z.c', 'zzzzzz', 1),
+(20, 'tttt', '', '', '', '', '', '', '', 1),
+(21, 'tttt', '', '', '', '', '', '', '', 1),
+(22, '1azni', 'YAO', 'KAN', '2023-07-22', '', 'a.webp', 'yao@gmail.com', 'yao', 1),
+(23, '0000', 'dely', 'ange', '2023-07-10', 'man', 'daciaduster2022.jpg', 'delyange.diomande@gmail.com', 'www', 1),
+(24, '0000', 'dely', 'ange', '2023-08-05', 'Alepe', '', 'azerty@gmail.com', 'azerty', 1);
 
 -- --------------------------------------------------------
 
@@ -91,76 +106,29 @@ CREATE TABLE `filiere` (
 --
 
 INSERT INTO `filiere` (`idFiliere`, `nomFiliere`) VALUES
-(1, 'Informatique'),
-(2, 'Gestion'),
-(3, 'Marketing'),
-(4, 'Informatique'),
-(5, 'Gestion'),
-(6, 'Marketing'),
-(7, 'Informatique'),
-(8, 'Gestion'),
-(9, 'Marketing'),
-(10, 'Informatique'),
-(11, 'Informatique'),
-(12, 'Gestion'),
-(13, 'Marketing'),
-(14, 'Informatique'),
-(15, 'Gestion'),
-(16, 'Marketing'),
-(17, 'Informatique'),
-(18, 'Gestion'),
-(19, 'Marketing'),
-(20, 'Informatique'),
-(21, 'Informatique'),
-(22, 'Gestion'),
-(23, 'Marketing'),
-(24, 'Informatique'),
-(25, 'Gestion'),
-(26, 'Marketing'),
-(27, 'Informatique'),
-(28, 'Gestion'),
-(29, 'Marketing'),
-(30, 'Informatique'),
-(31, 'Informatique'),
-(32, 'Gestion'),
-(33, 'Marketing'),
-(34, 'Informatique'),
-(35, 'Gestion'),
-(36, 'Marketing'),
-(37, 'Informatique'),
-(38, 'Gestion'),
-(39, 'Marketing'),
-(40, 'Informatique'),
-(41, 'Informatique'),
-(42, 'Gestion'),
-(43, 'Marketing'),
-(44, 'Informatique'),
-(45, 'Gestion'),
-(46, 'Marketing'),
-(47, 'Informatique'),
-(48, 'Gestion'),
-(49, 'Marketing'),
-(50, 'Informatique'),
-(51, 'Informatique'),
-(52, 'Gestion'),
-(53, 'Marketing'),
-(54, 'Informatique'),
-(55, 'Gestion'),
-(56, 'Marketing'),
-(57, 'Informatique'),
-(58, 'Gestion'),
-(59, 'Marketing'),
-(60, 'Informatique'),
-(61, 'Informatique'),
-(62, 'Gestion'),
-(63, 'Marketing'),
-(64, 'Informatique'),
-(65, 'Gestion'),
-(66, 'Marketing'),
-(67, 'Informatique'),
-(68, 'Gestion'),
-(69, 'Marketing'),
-(70, 'Informatique');
+(71, 'actuariat'),
+(72, 'statistique'),
+(73, 'miage'),
+(74, 'comptabilite');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `gestionnaire`
+--
+
+CREATE TABLE `gestionnaire` (
+  `id_gestionaire` int NOT NULL,
+  `mailGest` varchar(50) NOT NULL,
+  `mdpGest` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `gestionnaire`
+--
+
+INSERT INTO `gestionnaire` (`id_gestionaire`, `mailGest`, `mdpGest`) VALUES
+(1, 'root@root.root', 'root');
 
 -- --------------------------------------------------------
 
@@ -172,7 +140,7 @@ CREATE TABLE `inscrit` (
   `matElv` varchar(8) NOT NULL,
   `idNiveau` int NOT NULL,
   `idFiliere` int NOT NULL,
-  `DateInscription` date NOT NULL,
+  `DateInscription` varchar(18) NOT NULL,
   `status_Inscription` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
@@ -181,16 +149,73 @@ CREATE TABLE `inscrit` (
 --
 
 INSERT INTO `inscrit` (`matElv`, `idNiveau`, `idFiliere`, `DateInscription`, `status_Inscription`) VALUES
-('E001', 1, 1, '2022-11-01', 0),
-('E002', 1, 2, '2022-09-01', 1),
-('E003', 2, 1, '2022-05-01', 1),
-('E004', 2, 3, '2022-06-01', 1),
-('E005', 3, 1, '2022-07-01', 0),
-('E006', 1, 3, '2022-07-01', 0),
-('E007', 2, 2, '2022-06-01', 0),
-('E008', 3, 1, '2022-02-01', 0),
-('E009', 1, 2, '2022-02-01', 1),
-('E010', 2, 3, '2022-02-01', 1);
+('E001', 1, 1, '', 0),
+('E002', 1, 2, '', 1),
+('E003', 2, 1, '', 1),
+('E004', 2, 3, '', 1),
+('E005', 3, 1, '', 0),
+('E006', 1, 3, '', 0),
+('E007', 2, 2, '', 0),
+('E008', 3, 1, '', 0),
+('E009', 1, 2, '', 1),
+('E010', 2, 3, '', 1),
+('007', 1, 74, '06-05-2023', 0),
+('007', 1, 74, '06-05-2023', 0),
+('008', 1, 74, '24-05-2023', 0),
+('008', 1, 74, '24-05-2023', 0),
+('009', 2, 72, '25-06-2023', 0),
+('009', 2, 72, '25-06-2023', 0),
+('219', 2, 72, '25-06-2023', 0),
+('119', 2, 72, '25-06-2023', 0),
+('219', 2, 72, '25-06-2023', 0),
+('319', 2, 72, '25-06-2023', 0),
+('119', 2, 72, '25-06-2023', 0),
+('119', 2, 72, '25-06-2023', 0),
+('119', 2, 72, '25-06-2023', 0),
+('219', 2, 72, '25-06-2023', 0),
+('219', 2, 72, '25-06-2023', 0),
+('19', 2, 72, '25-06-2023', 0),
+('20', 2, 72, '25-06-2023', 0),
+('20', 2, 72, '25-06-2023', 0),
+('20', 2, 72, '25-06-2023', 0),
+('12jiini', 2, 72, '25-06-2023', 0),
+('1azni', 2, 72, '25-06-2023', 0),
+('22222', 2, 73, '11-11-111', 0),
+('4444', 4, 74, '44-44-444', 0),
+('4444', 4, 74, '44-44-444', 0),
+('5555', 2, 71, '55-55-555', 0),
+('5555', 2, 71, '55-55-555', 0),
+('5555', 2, 71, '55-55-555', 0),
+('5555', 2, 71, '55-55-555', 0),
+('5555', 2, 71, '55-55-555', 0),
+('5555', 2, 71, '55-55-555', 0),
+('5555', 2, 71, '55-55-555', 0),
+('666', 3, 74, '66-66-66', 0),
+('777', 4, 72, '77-77-77', 0),
+('777', 4, 72, '77-77-77', 0),
+('1234', 1, 74, '33-33-3333', 0),
+('AZERTY', 5, 74, 'sd', 0),
+('4444', 4, 74, '44-44-444', 0),
+('sqd', 1, 74, 'za', 0),
+('sqd', 1, 74, 'za', 0),
+('', 1, 74, '', 0),
+('1111', 1, 74, 'za', 0),
+('', 1, 74, '', 0),
+('', 1, 74, '', 0),
+('4444', 4, 74, '44-44-444', 0),
+('4444', 4, 74, '44-44-444', 0),
+('AZERTY', 1, 74, 'za', 0),
+('qsd', 1, 74, '30-06-2023', 0),
+('tttt', 1, 74, '', 0),
+('tttt', 1, 74, '', 0),
+('cvbn', 1, 74, 'za', 0),
+('AZERTY', 1, 74, '', 0),
+('AAAAAAAA', 3, 74, 'BBBB', 0),
+('XXXX', 1, 74, 'DDDD', 0),
+('ccc', 1, 74, '22-22-2222', 0),
+('ccc', 1, 74, '22-22-2222', 0),
+('AZERTY', 1, 74, '', 0),
+('betsa', 1, 73, '22-22-2222', 0);
 
 -- --------------------------------------------------------
 
@@ -213,27 +238,7 @@ INSERT INTO `niveau` (`idNiveau`, `libNiveau`, `idClasse`) VALUES
 (2, 'L2', 2),
 (3, 'L3', 3),
 (4, 'M1', 1),
-(5, 'M2', 2),
-(6, 'L1', 1),
-(7, 'L2', 2),
-(8, 'L3', 3),
-(9, 'M1', 1),
-(10, 'M2', 2),
-(11, 'L1', 1),
-(12, 'L2', 2),
-(13, 'L3', 3),
-(14, 'M1', 1),
-(15, 'M2', 2),
-(16, 'L1', 1),
-(17, 'L2', 2),
-(18, 'L3', 3),
-(19, 'M1', 1),
-(20, 'M2', 2),
-(21, 'L1', 1),
-(22, 'L2', 2),
-(23, 'L3', 3),
-(24, 'M1', 1),
-(25, 'M2', 2);
+(5, 'M2', 2);
 
 -- --------------------------------------------------------
 
@@ -268,7 +273,7 @@ ALTER TABLE `classe`
 -- Index pour la table `eleve`
 --
 ALTER TABLE `eleve`
-  ADD PRIMARY KEY (`matElv`),
+  ADD PRIMARY KEY (`idEleve`),
   ADD KEY `idSexe` (`idSexe`);
 
 --
@@ -276,6 +281,12 @@ ALTER TABLE `eleve`
 --
 ALTER TABLE `filiere`
   ADD PRIMARY KEY (`idFiliere`);
+
+--
+-- Index pour la table `gestionnaire`
+--
+ALTER TABLE `gestionnaire`
+  ADD PRIMARY KEY (`id_gestionaire`);
 
 --
 -- Index pour la table `inscrit`
@@ -309,10 +320,22 @@ ALTER TABLE `classe`
   MODIFY `idClasse` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT pour la table `eleve`
+--
+ALTER TABLE `eleve`
+  MODIFY `idEleve` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
 -- AUTO_INCREMENT pour la table `filiere`
 --
 ALTER TABLE `filiere`
-  MODIFY `idFiliere` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `idFiliere` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT pour la table `gestionnaire`
+--
+ALTER TABLE `gestionnaire`
+  MODIFY `id_gestionaire` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `niveau`
